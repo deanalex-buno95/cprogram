b99e77832c0fcfe905b68f46a79b15f614028955
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 16:13:11 +0800

    Deal with arrays via `main.c`.

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 #include <math.h>
 
 int main() {
-    char name[45];  // Create a string of up to 45 characters
-    printf("Enter your name: ");
-//    scanf("%s", name);  Original way of getting strings (problem with lines of strings with spaces)
-    fgets(name, 45, stdin);  // Get a string of characters with spaces (`stdin` is the console used for C, disadvantage is the unavoidable newline).
-    printf("Your name is %s.\n", name);
+    /* Standard array */
+    int luckyNumbers[] = {0,1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("%d\n", luckyNumbers[0]);  // Print integer at index 0
+    luckyNumbers[1] = 10;  // Modify integer at index 1.
+    printf("%d\n", luckyNumbers[1]);  // Print integer at index 1 after mutation.
+    /* Declared array with known length */
+    int unluckyNumbers[10];
+    unluckyNumbers[2] = 100;  // Add integer at index 2.
+    printf("%d\n", unluckyNumbers[2]);  // Print integer at index 2
+    printf("%d\n", unluckyNumbers[3]);  // Print integer at index 3
+    /* Strings (array of characters) */
+    char word[45] = "Array";
     return 0;
 }
