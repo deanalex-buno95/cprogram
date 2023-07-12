726f4e7b916d8ed0e632830ea24acf45cd351fc3
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 14:39:14 +0800

    Dealing with numbers and other Math-related functions via `main.c`.

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main() {
-    printf("Hello ");  // Basic printing
-    printf("World\n");  // End line
-    printf("\"Juan\"\n");  // Special characters
-    printf("%d%% success rate.\n", 90);  // Format specifier
-    printf("My favorite %s is %f.\n", "number", 95.69420);  // Format specifiers
-    int number = 95;
-    printf("My favorite number is %d.\n", number);  // Utilize variables
-    char letter = 'B';
-    printf("My favorite letter is %c.\n", letter);
+    printf("%f\n", 9.5);  // Simple number
+    printf("%f\n", 5.0 + 4.5);  // Addition
+    printf("%f\n", 5.0 - 4.5);  // Subtraction
+    printf("%f\n", 5.0 * 4.5);  // Multiplication
+    printf("%f\n", 5.0 / 4.5);  // Division
+    printf("%f\n", 5 + 4.5);  // Add integer and float returns float
+    printf("%d\n", 7 / 4);  // Divide integer by integer returns integer truncated
+    printf("%f\n", 5 / 4.0);  // Divide integer by float returns float
+    int num = 5;
+    printf("%d\n", num);  // Use variables
+    printf("%f\n", pow(2, 3));  // x^y
+    printf("%f\n", ceil(5.9));  // Round up to nearest whole number
+    printf("%f\n", floor(5.9));  // Round down to nearest whole number
     return 0;
 }
