33ca9d4fe2c02af4c388343bb77795ae3226610e
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 13:58:35 +0800

    Print statements using replaced variables (string and integer) and placeholders via `main.c`.

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char characterName[] = "Santos";  // String variable
-    int characterAge = 21;  // Integer variable
+    char characterName[] = "John";  // String variable
+    int characterAge = 55;  // Integer variable
     printf("My name is %s.\n", characterName);  // Use string placeholder
     printf("I am %d years old.", characterAge);  // Use integer placeholder
     return 0;
