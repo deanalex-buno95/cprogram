f5c4d26fc4409af3d7ac18f8e9719dfc932f7cee
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 14:55:02 +0800

    Dealing with constants via `main.c`.

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 #include <math.h>
 
 int main() {
-    /* Explanation of code */
-
-    /*
-    Insert commented-out code here…
-    printf("Hello World!");
-    */
-
-    printf("Comments are fun!");  // Inline comment
+    int num = 5;
+    printf("%d\n", num);  // Standard procedure
+    num = 8;
+    printf("%d\n", num);  // With modified variable
+    const int FAV_NUM = 95;
+    printf("%d\n", FAV_NUM);  // With variable that cannot be modified (throws error before build creation, different letter case for best practices)
+    printf("95\n");  // Special constant 1 (direct string input, piece of data unchanged)
+    printf("%d\n", 95);  // Special constant 2 (direct integer input, piece of data unchanged)
     return 0;
 }
