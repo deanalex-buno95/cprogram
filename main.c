8174741d8027d6cd1d7cb314b8505ca573efe56d
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-14 16:07:01 +0800

    Update `main.c` to learn about "While Loops".

--- main.c
+++ main.c
@@ -1,42 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-struct Student {
-    /* Attributes */
-    char name[45];
-    char major[45];
-    int birthYear;
-    double gpa;
-};
 
 
 int main() {
-    /* Create a struct of `Student` */
-    struct Student student1;
-    student1.birthYear = 2002;
-    student1.gpa = 6.42;
-    strcpy(student1.name, "Santos");
-    strcpy(student1.major, "BIT");
-
-    /* Create another struct of `Student` */
-    struct Student student2;
-    student2.birthYear = 2006;
-    student2.gpa = 5.25;
-    strcpy(student2.name, "Alexandra");
-    strcpy(student2.major, "Nursing");
-
-    /* Print output */
-    printf("_Student 1_\n");
-    printf("Age: %d\n", 2023 - student1.birthYear);
-    printf("GPA: %lf\n", student1.gpa);
-    printf("Name: %s\n", student1.name);
-    printf("Major: %s\n", student1.major);
-    printf("_Student 2_\n");
-    printf("Age: %d\n", 2023 - student2.birthYear);
-    printf("GPA: %lf\n", student2.gpa);
-    printf("Name: %s\n", student2.name);
-    printf("Major: %s\n", student2.major);
-
+    int value;
+    printf("Write a value: ");
+    scanf("%d", &value);
+    int numberOfSteps = 0;
+    printf("Step %d: %d\n", numberOfSteps, value);
+    while (value > 1) {
+        if (value % 2 == 1) {
+            value = 3 * value + 1;
+        }
+        else {
+            value /= 2;
+        }
+        numberOfSteps++;
+        printf("Step %d: %d\n", numberOfSteps, value);
+    }
+    printf("It takes %d steps to reach %d.\n", numberOfSteps, value);
     return 0;
 }
