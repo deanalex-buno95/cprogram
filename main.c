ed34d16859e6c1bc53b85494719209a84da3b81a
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-14 14:48:25 +0800

    Update `main.c` to learn about "If Statements".

--- main.c
+++ main.c
@@ -1,15 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-
-double cube(double num);  // Prototype
-
-int main() {
-    /* Note that the function called must be above the main, unless you prototype the function */
-    printf("Answer: %f", cube(5.0));
-    return 0;
+bool isLeapYear(int year) {
+    /* Use if-elseif-else conditions and && */
+    if (year % 100 == 0 && year % 400 == 0) {
+        // Leap year condition 1
+        return true;
+    }
+    else if (year % 100 != 0 && year % 4 == 0) {
+        // Leap year condition 2
+        return true;
+    }
+    else {
+        // Common year condition
+        return false;
+    }
 }
 
-double cube(double num) {
-    return num * num * num;
+bool isRightHeight(double height) {
+    /* Use if-else conditions and || */
+    if (height < 160.0 || height > 231.0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+int main() {
+    /* Variables for year */
+    int year1 = 2000;
+    int year2 = 1984;
+    int year3 = 1800;
+    int year4 = 1765;
+    int years[] = {year1, year2, year3, year4};
+    /* Use if-else conditions for year */
+    for (int i = 0; i < sizeof(years)/sizeof(years[0]); ++i) {
+        if (isLeapYear(years[i])) {
+            printf("%d is a leap year.\n", years[i]);
+        }
+        else {
+            printf("%d is a common year.\n", years[i]);
+        }
+    }
+    /* Variables for height */
+    double height1 = 159.0;
+    double height2 = 232.0;
+    double height3 = 206.0;
+    double heights[] = {height1, height2, height3};
+    /* Use if-else conditions for height */
+    for (int i = 0; i < sizeof(heights)/sizeof(heights[0]); ++i) {
+        if (isRightHeight(heights[i])) {
+            printf("%.2lf meters is just right.\n", heights[i]);
+        }
+        else {
+            printf("%.2lf meters is either too short or too tall.\n", heights[i]);
+        }
+    }
+    return 0;
 }
