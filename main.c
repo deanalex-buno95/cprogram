e28737e1ee2353fc76edd36de419d78534ef6916
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-15 17:24:38 +0800

    Update `main.c` to learn about "Writing ".

--- main.c
+++ main.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void lesson25() {
+    /**
+     * Lesson 25 — Writing Files
+     */
+    printf("* Lesson 25 *\n");
+    /* "w" permission */
+    // Write new file.
+    FILE * fPointer = fopen("../rggCharacters.txt", "w");  // Path not specified → File written in the same directory as the C build file.
+    // Add text to file.
+    fprintf(fPointer, "Kiryu,Red\nYagami,Blue\n");  // Note that another `fprintf`, or filling the string with something else, will overwrite the file.
+    printf("Kiryu and Yagami written into %s\n", "rggCharacters.txt");
+    // Close file.
+    fclose(fPointer);
+    /* "a" permission */
+    // Get file again, but with a different pointer and permission ("w" to "a").
+    FILE * anotherFPointer = fopen("../rggCharacters.txt", "a");  // Most file types (.html, .css, etc.) can be modified.
+    // Add more text to the file.
+    fprintf(anotherFPointer, "Goda,Yellow\nSaejima,Green\n");
+    printf("Goda and Saejima appended into %s\n", "rggCharacters.txt");
+    // Close file again.
+    fclose(anotherFPointer);
+}
+
 
 int main() {
-    /* Variables */
-    int age = 21;
-    double gpa = 6.42;
-    char grade = 'A';
-
-    /* Pointer variables */
-    int * pAge = &age;
-    double * pGpa = &gpa;
-    char * pGrade = &grade;
-
-    /* Physical memory addresses in RAM (%p means pointer) */
-    printf("Age address: %p\n", pAge);
-    printf("GPA address: %p\n", pGpa);
-    printf("Grade address: %p\n", pGrade);
-
-    /* De-referenced pointer variables */
-    printf("Age: %d\n", *pAge);
-    printf("GPA: %lf\n", *pGpa);
-    printf("Grade: %c\n", *pGrade);
-
+    /**
+     * All lessons
+     */
+    printf("------------------------------------------------------------------------------------------------\n");
+    lesson25();  // Writing files
+    printf("------------------------------------------------------------------------------------------------\n");
     return 0;
 }
